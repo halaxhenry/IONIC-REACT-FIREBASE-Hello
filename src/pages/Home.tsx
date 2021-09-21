568075a34cd9e20e05fa7bccfc7d653a3959b337
SeungchulHa <alaxhenry@gmail.com>
2021-09-21 18:55:53 +0900

    button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,63 +5,26 @@ import {
 	IonApp, 
 	IonTitle, 
 	IonToolbar, 
-	IonList, 
-	IonItem,
-	IonText,
-	IonAvatar,
-	IonLabel,
-	IonItemOptions,
-	IonItemSliding,
-	IonItemOption,
-
+	IonButton,
+	IonIcon,
 } from '@ionic/react';
+import {star} from 'ionicons/icons';
 import './Home.css';
-
-const arr = [
-	{
-		name:'Finn', 
-		desc: 'This guy rocks!'
-	},
-	{
-		name:'Han', 
-		desc: 'Trust me, I am a programmer'
-	},
-	{
-		name:'Rey', 
-		desc: 'I am done with it!'
-	},
-]
 
 const Home: React.FC = () => {
   return (
     <IonApp>
       <IonHeader>
-        <IonToolbar color="dark">
+        <IonToolbar>
           <IonTitle>Hello World!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonList>
-			{arr.map(elem => (
-				<IonItemSliding key={elem.name}>
-					<IonItem>
-						<IonAvatar>
-							<img src={`https://ionicframework.com/docs/demos/api/list/avatar-${elem.name.toLowerCase()}.png`} />
-						</IonAvatar>
-
-						<IonLabel className="ion-padding">
-							<h2>{elem.name}</h2>
-							<h3>{elem.desc}</h3>
-							<p>Some helper text!</p>
-						</IonLabel>
-					</IonItem>
-						<IonItemOptions side="end">
-							<IonItemOption onClick={() => alert('pressed deleted!')}>Delete</IonItemOption>
-						</IonItemOptions>
-				</IonItemSliding>
-			))}
-		</IonList>
-      </IonContent>
+		  <IonButton expand="full" color="secondary">
+			  <IonIcon slot="end" icon={star}></IonIcon>
+			  Hello World
+		  </IonButton>
+	  </IonContent>
     </IonApp>
   );
 };
